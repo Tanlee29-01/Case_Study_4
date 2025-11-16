cbe728f857e306b9935a25f1d79b8809b5161332
Tân Lê <leetan291@gmail.com>
2025-11-16 12:57:00 +0700

    Update Function.h

--- Case_Study_4/Function.h
+++ Case_Study_4/Function.h
@@ -1,52 +1,33 @@
 ﻿#pragma once
-#include<vector>
-#include<string>
-#include "DataTypes.h"
-#include "Tree_AVL.h"
-class Function
-{
+
+#include "Tree_AVL.h" 
+#include <vector>
+#include <string>
+using namespace std;
+
+class Function {
 public:
-	//Thêm
-	//Xóa
-	//Cập nhật
-	//Tìm kiếm
-	//Sắp xếp
-	Function();
-	//1. Thêm lớp
-	void Insert_Class();
-	//2. Xóa Lớp 
-	void Delete_Class();
-	//3. Cập nhật Lớp học
-	void Update_Class();
-	//4. Tìm theo ID
-	void Search_By_ID();
-	//5. Tìm theo Name
-	void Search_By_Name();
-	//6. In mã môn
-	void Show_Sub_ID();
-	//7. In tên môn
-	void Show_Sub_Name();
-	//8. In thời gian
-	void Show_Time();
-	//9. Test Hiệu năng
-	void Test_Performance();
-	~Function();
+    Function();
+    ~Function();
+
+    void Insert_Class();
+    void Delete_Class();
+    void Update_Class();
+    void Search_By_ID();
+    void Search_By_Name();
+    void Show_Sub_ID();
+    void Show_Sub_Name();
+    void Show_Time();
+    void Test_Performance();
 
 private:
-	/*
-	================
-	=  ID,Tên,Time =
-	================ 
-	*/ 
-	vector<Class*> List_ID;
-	vector<string> List_Name;
-	vector<int> List_Time;
-	AVLTree<CompareByMaMon> tree_id;
-	AVLTree<CompareByTenMon> tree_name;
-	AVLTree<CompareByThoiGian> tree_time;
+    vector<Class*> danhSachLop;
 
-	Time Input_Time();
+    AVLTree<CompareByMaMon> treeTheoMa;
+    AVLTree<CompareByTenMon> treeTheoTen;
+    AVLTree<CompareByThoiGian> treeTheoThoiGian;
 
-	Class* Pointer_Search_with_ID(const string& Sub_ID);
+    Time Input_Time();
+
+    Class* Pointer_Search_with_ID(const std::string& Sub_ID);
 };
-
